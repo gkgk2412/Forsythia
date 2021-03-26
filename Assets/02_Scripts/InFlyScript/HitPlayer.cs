c43ee8127eb9d9c0cdae47ab37aad9bed2334f45
HuiJin son <54790406+gkgk2412@users.noreply.github.com>
2021-03-27 01:07:49 +0900

    Forsythia
    2021.03.27, Re

---
+++ Assets/02_Scripts/InFlyScript/HitPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlayer : MonoBehaviour
+{
+    float SunScore = 50;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player") //햇살이 Player 태그와 충돌하면
+        {
+         //  if (InGamePlayer.BigSuccess)
+          //  {
+                ScoreManager.Score += SunScore * 1.5f;
+          //  }
+           //else if (InGamePlayer.Success)
+          //  {
+                ScoreManager.Score += SunScore;
+          //  }
+        }
+    }
+}
---
+++ Assets/02_Scripts/InGameScript/ClickButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickButton : MonoBehaviour
+{
+    public GameObject target;
+
+    public static bool IsStrong = false; //강하게를 눌렀는지
+    public static bool IsWeak = false; //약하게를 눌렀는지
+    public static bool IsLeftJump = false; //왼쪽 도약 키를 눌렀는지
+    public static bool IsRightJump = false; //오른쪽 도약 키를 눌렀는지
+    public static bool IsStop = false; //옵션을 눌렀는지
+
+    public static bool isFIrstJump = true;
+
+    public void Strong()
+    {
+       IsStrong = true;
+
+    }
+
+    public void Weak()
+    {
+        IsWeak = true;
+
+
+
+
+    }
+
+    public void LeftJump()
+    {
+       IsLeftJump = true;
+    }
+
+    public void RightJump()
+    {
+       IsRightJump = true;
+    }
+
+    public void TimeNotMove()
+    {
+        if (IsStop)
+            IsStop = false;
+        else if (!IsStop)
+            IsStop = true;
+    }
+
+    public void IsMain()
+    {
+        SceneManager.LoadScene("TestMain");
+    }
+
+    public void Again()
+    {
+        SceneManager.LoadScene("TestInGame");
+    }
+}
